8cd9badcd90acb335d6ae4c4af1ad15d32914176
simran-22 <simranpreet.3d@gmail.com>
2022-10-14 10:21:33 +0530

    added new comp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import CompA from "./CompA";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      Hello World
+      <CompA />
+    </div>
+  );
 }
 
 export default App;
